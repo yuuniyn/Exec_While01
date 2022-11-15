123463c2103dcb58671543ab4aa0041c59a95f22
Yun ni <erknard2015@gmail.com>
2022-11-16 03:15:16 +0800

    加入專案檔案。

---
+++ Q1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int i = 1;
+            while (sum < 105)
+            {
+                sum += i;
+                i++;
+            }
+
+            Console.WriteLine("1 + 2 + 3 + ..... + N < 105");
+            Console.WriteLine("=> N = " + (i - 2));
+        }
+    }
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            const int number = 300;
+            int i = 1;
+
+            while (i <= number)
+            {
+                if (number % i == 0)
+                    sb.Append(i + ", ");
+                i++;
+            }
+
+            Console.WriteLine(sb.ToString().Substring(0, sb.ToString().Length - 2));
+        }
+    }
+}
